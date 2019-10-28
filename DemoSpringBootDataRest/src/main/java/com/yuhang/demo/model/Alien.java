64990a633ae34f6d97dd5c7e1b3b64fb317e9f25
YUHANG ZHAO <zhaoy36@mcmaster.ca>
2019-10-28 13:41:36 -0400

    Update Class Alien

--- DemoSpringBootDataRest/src/main/java/com/yuhang/demo/model/Alien.java
+++ DemoSpringBootDataRest/src/main/java/com/yuhang/demo/model/Alien.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 public class Alien {
 		// YH: @Id specifies the primary key
 		@Id
-		@GeneratedValue(strategy=GenerationType.IDENTITY) 
+		@GeneratedValue(strategy=GenerationType.AUTO) 
 		private int aid;
 		private String aname;
 		private int points;
